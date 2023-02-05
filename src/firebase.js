523d70642b5866466a3ec8940550269d82e655f7
Hassan Kazi <andaijja@gmail.com>
2023-02-05 18:58:06 -0500

    rc

--- src/firebase.js
+++ src/firebase.js
@@ -8,17 +8,6 @@ import { firebaseConfig } from "./config";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBkZ6R_xadbvGLq9pGGxyLYwHK51gzBehM",
-//     authDomain: "tech-inc-e0025.firebaseapp.com",
-//     projectId: "tech-inc-e0025",
-//     storageBucket: "tech-inc-e0025.appspot.com",
-//     messagingSenderId: "703757534699",
-//     appId: "1:703757534699:web:b65e21f13e4a5dde1585ae",
-//     measurementId: "G-XQG33WF3VR"
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
